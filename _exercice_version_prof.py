9faf2882fd4f3ab37fd4a769bf85e7dfff1039ca
kegaub <kevin.gauthier.22.06@gmail.com>
2021-05-17 11:18:49 -0400

    Update _exercice_version_prof.py

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -4,76 +4,44 @@
 
 def order(values: list = None) -> list:
     if values is None:
-        # TODO: Demander les valeurs ici
-        values = []
-        while len(values) < 10:
-            values.append(input("Please enter a value\n"))
+        # TODO: demander les valeurs ici
+        pass
 
-    return sorted(values)
+    return []
 
 
 def anagrams(words: list = None) -> bool:
     if words is None:
-        # TODO: Demander les mots ici
-        words = []
-        while len(words) < 2:
-            words.append(input("Please enter string\n"))
+        # TODO: demander les mots ici
+        pass
 
-    return sorted(words[0]) == sorted(words[1])
+    return False
 
 
 def contains_doubles(items: list) -> bool:
-
-    return len(set(items)) != len(items)
+    return False
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    best_student = dict()
-    for key, value in student_grades.items():
-        average = sum(value)/len(value)
-
-        if len(best_student) == 0 or list(best_student.values())[0] < average:
-            best_student = {key: average}
-
-    return best_student
-
+    return {}
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
 
-    frequency = dict()
-    for letter in sentence:
-        frequency[letter] = sentence.count(letter)
-
-    sorted_keys = sorted(frequency, key=frequency.__getitem__, reverse=True)
-    for key in sorted_keys:
-        if frequency[key] > 5:
-            print(f"Le caractère {key} revient {frequency[key]} fois.")
-
-    return frequency
+    return {}
 
 
 def get_recipes():
-    # TODO: Demander le nom d'une recette, puis ses ingrédients et enregistrer dans une structure de données
-    name = input("Quel est le nom de votre recette?\n")
-    ingredient = input("Entrer la liste d'ingrédients? Séparer les ingrédiants par une ,\n").split(",")
-
-    return {name: ingredient}
+    # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
+    pass
 
 
 def print_recipe(ingredients) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    name = input("Quel est le nom de votre recette?\n")
-
-    if name in ingredients:
-        print(ingredients[name])
-    else:
-        print("Cette recette n'est pas dans le livre!")
-        print_recipe(ingredients)
-
+    pass
 
 
 def main() -> None:
